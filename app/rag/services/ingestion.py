d024bc0f877224486ab6e31c0f4777fb31b3ddd2
yash meshram <yashmeshram4102000@gmail.com>
2026-04-23 20:19:00 +0530

    modified ingestion process

--- app/rag/services/ingestion.py
+++ app/rag/services/ingestion.py
@@ -3,24 +3,6 @@ from langchain_core.documents import Document
 from db.mongo import mongodb_vector_store
 
 
-def prepare_document(user_id: str, chunks: List[Document]) -> List[Document]:
-    for i, chunk in enumerate(chunks):
-        chunk.metadata.update({
-            "user_id": user_id,
-            "file_type": chunk.metadata['file_type'],
-            "file_name": chunk.metadata['file_name'],
-            "content_type": chunk.metadata['content_type'],
-            "chunk_id": i
-        })
-    return chunks
-
-
 def store_document(user_id: str, chunks: List[Document]):
-    # prepare document
-    document = prepare_document(user_id, chunks)
-    
     # adding document into vector store
-    mongodb_vector_store.add_documents(document)
-    
-    
-    
+    mongodb_vector_store.add_documents(chunks)
