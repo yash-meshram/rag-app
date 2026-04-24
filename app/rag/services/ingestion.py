2a2bcb21f0d0b7499408d220418f010f3bfbdd98
yash meshram <yashmeshram4102000@gmail.com>
2026-04-24 23:33:30 +0530

    refactor: update store_document function to load and chunk documents from file

--- app/rag/services/ingestion.py
+++ app/rag/services/ingestion.py
@@ -1,8 +1,14 @@
-from typing import List
-from langchain_core.documents import Document
-from db.mongo import mongodb_vector_store
+from app.rag.services.process_documents import load_document
+from app.rag.services.chunking import chunk_document
+from app.rag.db.mongo import mongodb_vector_store
 
 
-def store_document(user_id: str, chunks: List[Document]):
-    # adding document into vector store
+def store_document(file_path: str, user_id: str):
+    # loading the document
+    document = load_document(file_path = file_path, user_id = user_id)
+    
+    # splitting the documenst into chunks
+    chunks = chunk_document(document = document)
+    
+    # adding chunks into vector store
     mongodb_vector_store.add_documents(chunks)
