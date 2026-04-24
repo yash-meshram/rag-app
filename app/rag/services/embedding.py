20faa53185aabdff5d34999373fdb5f7ee4ccb7a
yash meshram <yashmeshram4102000@gmail.com>
2026-04-24 23:31:28 +0530

    remove unused imports and commented code in embedding.py

--- app/rag/services/embedding.py
+++ app/rag/services/embedding.py
@@ -1,15 +1,9 @@
-from typing import List
-import numpy as np
-from sentence_transformers import SentenceTransformer
+# from sentence_transformers import SentenceTransformer
 import os
 from langchain_huggingface import HuggingFaceEmbeddings
 
-embeding_model = os.getenv("EMBEDDING_MODEL")
+# embeding_model = os.getenv("EMBEDDING_MODEL")
 huggingface_embeding_model = os.getenv("HUGGINGFACE_EMBEDDING_MODEL")
 
 embedding = HuggingFaceEmbeddings(model_name = huggingface_embeding_model)
-model = SentenceTransformer(embeding_model)
-
-def get_embedding(texts: List[str]) -> np.ndarray:
-    embeddings = model.encode(texts, batch_size = 32, normalize_embeddings = True)
-    return embeddings
+# model = SentenceTransformer(embeding_model)
