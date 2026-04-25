4cfaa75817a6ddeae072f60318b639fe5c9d5af4
yash meshram <yashmeshram4102000@gmail.com>
2026-04-25 23:23:32 +0530

    fix: update query endpoint to correctly handle document retrieval and image response

--- app/rag/routes/query.py
+++ app/rag/routes/query.py
@@ -9,14 +9,14 @@ llm = get_model()
 @router.post("/query")
 async def query(request: QuestionRequest):
 
-    if request.user_id and request.file_name:
-        docs = retrieve_docs(query = request.question, user_id = request.user_id, file_name = request.file_name)
+    if request.file_name:
+        docs, images = retrieve_docs(query = request.question, user_id = request.user_id, file_name = request.file_name)
         prompt = f"""
         Query: {request.question}
         Referance: {[doc.page_content for doc in docs]}
         """
         response = llm.invoke([prompt])
+        return {"response": response.text, "images": images}
     else:
         response = llm.invoke([request.question])
-
-    return {"response": response.text}
+        return {"response": response.text}
